0c5eab6041d830fd4de1e86955bf0bf0c6fd60a7
Ghareeb29 <sonic_unleashed@live.com>
2024-08-17 22:00:39 +0300

    NoSQL task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for Python function which lists all documents in a collection"""
+def list_all(mongo_collection):
+    """
+    Lists all documents in a MongoDB collection.
+    
+    Args:
+    mongo_collection: A pymongo collection object
+    
+    Returns:
+    A list of all documents in the collection,
+    or an empty list if no documents are found.
+    """
+    return list(mongo_collection.find())
